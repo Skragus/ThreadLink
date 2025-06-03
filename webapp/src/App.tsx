472f4b01e4f1fb71e1648625d5ca557712b49e97
Skragus <104354593+Skragus@users.noreply.github.com>
2025-06-03 21:58:01 +0000

    Set up ThreadLink web application

---
+++ webapp/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ThreadLink from './components/ThreadLink';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <ThreadLink />
+    </div>
+  );
+}
+
+export default App;
